a960b65ff1f3434d8708c3b4031a90d8e557caf3
will-castilho <willian.cp@edu.udesc.br>
2024-04-29 19:37:15 -0300

    Classe Pessoa e respectivo teste dentro do mesmo pacote

---
+++ src/domain/TestaPacote.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class TestaPacote {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+
+        p.nome = "Zé da Silva";
+        System.out.println(p.nome);
+    }
+}
